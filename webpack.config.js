6b783701d724eb1b047f805aad48fb5d46769361
Filippo Massarelli <filippo.massarelli@gmail.com>
2020-08-06 19:21:09 +0200

    practice with webpack rules (loaders) and entry point, and webpack plugins (html-webpack-plugin). Explore generated dist. Change server to point to dist when initiating a new express instance

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,32 @@
 const path = require("path");
 const webpack = require("webpack");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   entry: "./src/client/index.js",
   mode: "development",
+  module: {
+    rules: [
+      {
+        test: "/.js$/",
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: "/.js$/",
+        exclude: /node_modules/,
+        loader: "eslint-loader",
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: "file-loader",
+      },
+    ],
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
+  ],
 };
