f62b768b98c22ce9f530837720f3865b032435b7
Filippo Massarelli <filippo.massarelli@gmail.com>
2020-08-11 14:55:11 +0200

    Configure production optimization using Terse, OptimizeCSSAssets and MiniCssExtract Plugins. Evaluate reduction in page load size due to minified assets by comparing run build-dev and build-prod to localhost 8080 and 8081 respectively and checking the network tab of the chrome dev tools. Result: 30% smaller in production

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,9 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
   entry: "./src/client/index.js",
@@ -8,6 +11,9 @@ module.exports = {
   output: {
     libraryTarget: "var",
     library: "Client",
+  },
+  optimization: {
+    minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
   },
 
   module: {
@@ -19,7 +25,7 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },
@@ -28,5 +34,6 @@ module.exports = {
       template: "./src/client/views/index.html",
       filename: "./index.html",
     }),
+    new MiniCssExtractPlugin({ filename: "[name].css" }),
   ],
 };
