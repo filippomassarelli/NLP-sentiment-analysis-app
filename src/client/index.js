15772a8b06f0306190788d93d6d81ef554ff5f5f
Filippo Massarelli <filippo.massarelli@gmail.com>
2020-08-17 18:27:29 +0200

    change all styling using sass

--- src/client/index.js
+++ src/client/index.js
@@ -7,5 +7,6 @@ import "./styles/base.scss";
 import "./styles/footer.scss";
 import "./styles/form.scss";
 import "./styles/header.scss";
+import "./styles/results.scss";
 
 export { postData, handleSubmit, updateUI };
--- src/client/js/updateUI.js
+++ src/client/js/updateUI.js
@@ -7,20 +7,20 @@ function updateUI(data) {
   let resultsHTML;
   if (data.status.code === "0") {
     resultsHTML = `
-                <h1>Results:</h1>
-                <ul>
-                    <li>${data.agreement}</li>
-                    <li>${data.irony}</li>
-                    <li>${data.confidence}% CONFIDENCE</li>
-                    <li>${data.subjectivity}</li>
-                </ul>
-                `;
+                <div>
+                <h1>Our analysis show that:</h1>
+                <p><i class="fas fa-hand-point-right"></i>The polarity of different elements of the text are in <span>${data.agreement.toLowerCase()}</span></p>
+                <p><i class="fas fa-hand-point-right"></i>The text is prevalently <span>${data.subjectivity.toLowerCase()}</span> and <span>${data.irony.toLowerCase()}</span></p>
+                <p><i class="fas fa-hand-point-right"></i>We are <span>${data.confidence.toLowerCase()}%</span> confident about these resulst</p>
+                </div>`;
   } else {
     resultsHTML = `
+                <div>
                 <h1>Sorry!</h1>
+                <i class="fas fa-car-crash"></i>
                 <p>${data.status.msg}</p>
                 <p>(error code${data.status.code})</p>
-                `;
+                </div>`;
   }
 
   //add resultsHTML to DOM
